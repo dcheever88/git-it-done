93dbe4e350c9c3a0004a1eec4163a357a8a9876c
dcheever <cheeverdanny@gmail.com>
2021-03-31 17:48:19 -0500

    link for 30 plus issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -11,11 +12,18 @@ var getRepoIssues = function(repo) {
             .then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request.");
         }
+
     });
+
 };
 
 var displayIssues = function(issues) {
@@ -23,7 +31,7 @@ var displayIssues = function(issues) {
         issueContainerEl.textContent = "This repo has no open issues";
         return;
     }
-    
+
     for (var i = 0; i <issues.length; i++) {
         // create a link element to take users to the issue on gitHub
         var issueEl = document.createElement("a");
@@ -53,6 +61,19 @@ var displayIssues = function(issues) {
 
         issueContainerEl.appendChild(issueEl);
     }
-}
+};
 
-getRepoIssues("dcheever88/taskmaster-pro");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
